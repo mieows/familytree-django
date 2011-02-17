0300a1cee00ce9ce9fcac05f63352598b01e9212
Kat Farrell <kat.farrell@redbrick.dcu.ie>
2011-02-17 22:49:23 +0000

    create an encapsulation class that figures out info about the entire family

--- views.py
+++ views.py
@@ -2,9 +2,28 @@
 from django.shortcuts import render_to_response, get_object_or_404
 from django.http import Http404
 from familytree.models import *
-# Create your views here.
+from familytree.helper import *
+
+#created this as a test
+class theFamily():
+	#fast objects
+	familyDir = Person.objects.all()
+	males  = familyDir.filter(sex='M')
+	females = familyDir.filter(sex='F')
+
+	#Names info
+	firstname_distinct = familyDir.values('firstname').distinct()
+	surname_distinct = familyDir.values('surname').distinct()        
+	middlename1_distinct = familyDir.values('middlename1').distinct()    
+	middlename2_distinct = familyDir.values('middlename2').distinct() 
+
+	firstname_reuse = name_count(familyDir.values('firstname'))
+	surname_reuse = name_count(familyDir.values('surname'))
+	middlename_reuse = name_count(familyDir.values('middlename1') + familyDir.values('middlename2')))
+	all_reuse = name_count(familyDir.values('firstname') + familyDir.values('middlename1') + familyDir.values('middlename2'))
+
 def index(request):
         people = Person.objects.all().order_by('-created_date')[:5]
-
-        return render_to_response('familytree/index.html', {'people': people})
+	about = theFamily
+        return render_to_response('familytree/index.html', {'people': people, 'about': about})
 
